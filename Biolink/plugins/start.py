3b6e4c313e5b52a80bab79d79adebf41de459e7a
𝐒𝐏𝐀𝐌 𝐁𝐎𝐓 𝐂𝐎𝐃𝐄𝐑 🚩 <light12035@gmail.com>
2025-11-02 19:18:13 +0530

    Update start.py

--- Biolink/plugins/start.py
+++ Biolink/plugins/start.py
@@ -17,13 +17,13 @@ If you need help or updates, check the buttons below 👇"""
         
         buttons = InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("➕ Add me to your group", url="https://t.me/Aaradhyasongbot?startgroup=true")],
-                [InlineKeyboardButton("📢 Updates Channel", url="https://t.me/shivang_xd")],
+                [InlineKeyboardButton("➕ Add me to your group", url="https://t.me/Biolink_removalbot?startgroup=true")],
+                [InlineKeyboardButton("📢 Updates Channel", url="https://t.me/shona_bots")],
                 [InlineKeyboardButton("❓ Help & Commands", callback_data="help")]
             ]
         )
 
-        photo_url = "https://graph.org/file/fab32af0223055637d4a6-356f444b3e7453b4d6.jpg"
+        photo_url = "https://files.catbox.moe/rv6vxm.jpg"
 
         try:
             # Group mein photo bhejna kabhi kabhi permission issue de sakta hai
